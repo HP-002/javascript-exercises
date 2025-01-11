8409d6006f859b3cab2d222b572ac079b7fd0d7a
hetpatel <hetfaldu19@gmail.com>
2025-01-11 22:33:56 +0530

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,47 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currYear = new Date().getFullYear();
 
+    let oldest;
+    let oldestAge = 0;
+    let currAge;
+
+    for(const person of people) {
+        if(person["yearOfDeath"] == undefined) {
+            currAge = currYear - person["yearOfBirth"];
+        } else {
+            currAge = person["yearOfDeath"] - person["yearOfBirth"];
+        }
+
+        if(currAge > oldestAge) {
+            oldestAge = currAge;
+            oldest = person;
+        }
+    }
+
+    return oldest;
 };
+
+
+ 
+const people = [
+        {
+          name: "Carly",
+          yearOfBirth: 1942,
+          yearOfDeath: 1970,
+        },
+        {
+          name: "Ray",
+          yearOfBirth: 1962,
+          yearOfDeath: 2011,
+        },
+        {
+          name: "Jane",
+          yearOfBirth: 1912,
+          yearOfDeath: 1941,
+        },
+      ]
+findTheOldest(people)
+
 
 // Do not edit below this line
 module.exports = findTheOldest;

d596d2c3a4144e5c7254c00b59c88f6be178f243
hetpatel <hetfaldu19@gmail.com>
2025-01-11 16:06:11 +0530

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseStr = ''
+    while(str != '') {
+        reverseStr = str.charAt(0) + reverseStr;
+        str = str.slice(1);
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line

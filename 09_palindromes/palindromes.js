2d9934b701134177b394b5e8d7fa6809c9268e86
hetpatel <hetfaldu19@gmail.com>
2025-01-11 22:01:15 +0530

    Complete 09_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let digits = '0123456789';
+    let alphabets = 'abcdefghijklmonpqrstuvwxyz';
+    let start = 0, end = str.length-1;
+    let charStart, charEnd;
+    str = str.toLowerCase();
+    while(start != end) {
+        charStart = str.charAt(start);
+        if(!digits.includes(charStart) && !alphabets.includes(charStart)) {
+            start++;
+            continue;
+        }
 
+        charEnd = str.charAt(end);
+        if(!digits.includes(charEnd) && !alphabets.includes(charEnd)) {
+            end--;
+            continue;
+        }
+
+        if(charStart !== charEnd) {
+            return false;
+        }
+        start++; end--;
+    }
+
+    return true;
 };
+
+palindromes('r3ace3car')
 
 // Do not edit below this line
 module.exports = palindromes;

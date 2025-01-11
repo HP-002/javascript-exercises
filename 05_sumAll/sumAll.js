74bef96334f8e920f247da1f96986775811915d0
hetpatel <hetfaldu19@gmail.com>
2025-01-11 18:49:56 +0530

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(!Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    }
 
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+
+    let lower = (start < end) ? start : end;
+    let higher = (start > end) ? start : end;
+    let sum = 0;
+
+    while(lower <= higher) {
+        sum += lower;
+        lower++;    
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

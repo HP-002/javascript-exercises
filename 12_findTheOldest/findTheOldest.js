d9579b80f4bc0af8c5423873374da00717ffd9a8
hetpatel <hetfaldu19@gmail.com>
2025-01-11 22:34:51 +0530

    Remove extra test from findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -21,27 +21,5 @@ const findTheOldest = function(people) {
     return oldest;
 };
 
-
- 
-const people = [
-        {
-          name: "Carly",
-          yearOfBirth: 1942,
-          yearOfDeath: 1970,
-        },
-        {
-          name: "Ray",
-          yearOfBirth: 1962,
-          yearOfDeath: 2011,
-        },
-        {
-          name: "Jane",
-          yearOfBirth: 1912,
-          yearOfDeath: 1941,
-        },
-      ]
-findTheOldest(people)
-
-
 // Do not edit below this line
 module.exports = findTheOldest;

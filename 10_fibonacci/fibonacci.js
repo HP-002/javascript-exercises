6efea6c2f5313f4d5cb1e6e20dfea8b1c2559f5f
hetpatel <hetfaldu19@gmail.com>
2025-01-11 22:08:05 +0530

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    // 0 1 1 2 3
+    pos = Number.parseInt(pos);
+    if(pos < 0) {
+        return "OOPS"
+    }
 
+    if(pos == 0) {
+        return 0;
+    }
+    if(pos == 1) {
+        return 1;
+    }
+
+    let a = 0;
+    let b = 1;
+    let c;
+    while(pos != 1) {
+        c = a + b;
+        a = b;
+        b = c;
+        pos--;
+    }
+
+    return b;
 };
 
 // Do not edit below this line

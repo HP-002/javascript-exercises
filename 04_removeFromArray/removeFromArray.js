c50a116615b32e51d59914a51ab380511d45f6a5
hetpatel <hetfaldu19@gmail.com>
2025-01-11 16:16:09 +0530

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let filteredArray = [];
+    outer:
+    for(const item of array) {
+        for(const target of toRemove) {
+            if(item === target) {
+                continue outer;
+            }
+        }
+        filteredArray.push(item);
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line

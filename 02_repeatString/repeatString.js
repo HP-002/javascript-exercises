294519c8b2018400108f2810407d34b3d1f8bca1
hetpatel <hetfaldu19@gmail.com>
2025-01-11 16:00:31 +0530

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if(n < 0) {
+        return 'ERROR';
+    }
 
+    let repeatedString = "";
+    while(n) {
+        repeatedString += str;
+        n--;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
